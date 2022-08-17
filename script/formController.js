d94f58aefb3cf358dfd2887a2652e4bf2db3bdb6
Majoxs <majoxs@gmail.com>
2022-08-17 15:46:30 +0500

    Implementation of sending data for editing and filtering

--- script/formController.js
+++ script/formController.js
@@ -2,8 +2,8 @@ import { API_URI } from './const.js';
 import { category, form, modal } from './elems.js';
 import { closeModal } from './modalController.js';
 import { showPreview } from './previewController.js';
-import { getCategory, getGoods, postGoods } from './serviceAPI.js';
-import { renderRow } from './tableView.js';
+import { editGoods, getCategory, getGoods, postGoods } from './serviceAPI.js';
+import { renderRow, editRow } from './tableView.js';
 import { toBase64 } from './utils.js';
 
 const updateCategory = async () => {
@@ -39,10 +39,17 @@ export const formController = () => {
       delete data.image;
     }
 
-    const goods = await postGoods(data);
-    renderRow(goods);
+    if (data.imagesave) {
+      const goods = await editGoods(data);
+      editRow(goods);
+    } else {
+      const goods = await postGoods(data);
+      renderRow(goods);
+    }
+
     closeModal(modal, 'd-block');
-  });
+    updateCategory();
+  })
 };
 
 export const fillingForm = async (id) => {
@@ -53,5 +60,6 @@ export const fillingForm = async (id) => {
   form.display.value = display;
   form.price.value = price;
   form.imagesave.value = image;
-  showPreview(`${API_URI}${image}`);
+  form.identificator.value = id;
+  showPreview(`${API_URI}${image}`)
 };
